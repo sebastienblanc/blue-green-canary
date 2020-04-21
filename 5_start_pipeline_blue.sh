e00dcd9872313b40a311fc413c35cf8141f8ed91
sebastienblanc <scm.blanc@gmail.com>
2020-04-21 21:07:03 +0200

    sebi change

--- 5_start_pipeline_blue.sh
+++ 5_start_pipeline_blue.sh
@@ -3,7 +3,7 @@
 tkn pipeline start bgc-build-deploy \
  --param="appName=blue"  \
  --param="color=#6bbded"  \
- --param="message=Hello"  \
+ --param="message=What"  \
  --resource="appSource=bgc-git-source" \
  --resource="appImage=bgc-image" \
  --showlog

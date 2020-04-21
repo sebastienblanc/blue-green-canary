c35cd26a77b5dbce0e74362c2253026f228dbf69
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-04-21 22:20:10 +0530

    WIP- Pipelines

--- 5_start_pipeline_blue.sh
+++ 5_start_pipeline_blue.sh
@@ -2,6 +2,9 @@
 
 tkn pipeline start bgc-build-deploy \
  --param="mavenMirrorUrl=http://nexus.rhd-workshop-infra:8081/nexus/content/groups/public"  \
+ --param="appName=blue"  \
+ --param="color='#6bbded'"  \
+ --param="message=Hello"  \
  --param="kubernetesResourceFile=/workspace/source/src/main/kubernetes/Deployment_blue.yml" \
  --resource="appSource=bgc-git-source" \
  --resource="appImage=bgc-image" \

6666dbfa1f827c8c21607083535758211185ff53
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-04-21 23:24:01 +0530

    Fixed pipelines

--- 5_start_pipeline_canary.sh
+++ 5_start_pipeline_canary.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 tkn pipeline start bgc-build-deploy \
- --param="appName=blue"  \
- --param="color=#6bbded"  \
- --param="message=Hello"  \
+ --param="appName=canary"  \
+ --param="color=#f2f25e"  \
+ --param="message=Hola"  \
  --resource="appSource=bgc-git-source" \
  --resource="appImage=bgc-image" \
  --showlog
--- 5_start_pipeline_green.sh
+++ 5_start_pipeline_green.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 tkn pipeline start bgc-build-deploy \
- --param="appName=blue"  \
- --param="color=#6bbded"  \
- --param="message=Hello"  \
+ --param="appName=green"  \
+ --param="color=#5bbf45"  \
+ --param="message=Bonjour"  \
  --resource="appSource=bgc-git-source" \
  --resource="appImage=bgc-image" \
  --showlog
